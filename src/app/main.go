cade7d5a6820145e0d9dcdb836d7c20f0de1bada
thaonx.it@gmail.com <thao.nguyen@lazada.com>
2018-10-12 04:07:46 +0700

    revise log

--- src/app/main.go
+++ src/app/main.go
@@ -99,7 +99,7 @@ func download(outputDir, cubeId string) (err error) {
 	json.NewDecoder(r.Body).Decode(&gameInfo)
 	code := gameInfo.GetString("code")
 	if code != "1" {
-		log.Println(cubeId, gameInfo.GetMap("data"))
+		log.Println(cubeId, gameInfo)
 		return nil
 	}
 
